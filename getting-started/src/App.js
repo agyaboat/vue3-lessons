41263eefb0228a806b0949e361340b9cda8af016
Bright, EliteBook <brightboateng003@gmail.com>
2025-08-12 09:55:36 +0000

    getting-started 1.1

--- getting-started/src/App.js
+++ getting-started/src/App.js
@@ -1,20 +1,30 @@
 import HelloWorld from "./HelloWorld.js"
-import {ref} from 'vue'
+import {ref, reactive} from 'vue'
 export default {
   name: 'App',
   data(){
     const x = ref(0);
+    const a = {x:10};
+    const b = reactive(a);
+    const c = reactive(a);
     return {
-        x
+        x,
+        a,
+        b,
+        c
     }
   },
   components: {
     HelloWorld
   },
   template: `
-    <div style="background:red">
+    <div style="" :id="x">
       <HelloWorld msg="Welcome to Your Vue.js App"/>
       <button @click="x++">{{ x }}</button>
-    </div>
+      <button @click="b.x++">{{ b }}</button>
+      <div>Yet C value is {{ c }}</div>
+      <div> {{ b===c }}</div>
+      <div> {{ a }}</div>
+      </div>
   `
 }
